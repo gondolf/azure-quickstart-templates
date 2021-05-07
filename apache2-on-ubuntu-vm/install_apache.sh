4d2429a18893cea98695a5d9b3cd51b73acb115d
gondolf <gaescajadillo@gmail.com>
2021-05-07 13:33:25 -0500

    Update install_apache.sh
    apache dependencies fixed

--- apache2-on-ubuntu-vm/install_apache.sh
+++ apache2-on-ubuntu-vm/install_apache.sh
@@ -4,8 +4,8 @@ apt-get -y update
 
 # install Apache2
 apt-get -y install apache2
-# apt-get -y install mysql-server
-# apt-get -y install php libapache2-mod-php php-mysql
+apt-get -y install mysql-server
+apt-get -y install php libapache2-mod-php php-mysql
 # write some HTML
 vmmetadata="curl -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-04-02""
 echo "<center><h1> Computer: $(hostname) </h1><br/>$($vmmetadata)</center>" > /var/www/html/index.html
