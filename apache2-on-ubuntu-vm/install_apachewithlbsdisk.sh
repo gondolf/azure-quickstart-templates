73c7d5c2868f3b49af39336bab247ecf71c368d8
gondolf <gaescajadillo@gmail.com>
2021-05-10 03:11:58 -0500

    Update install_apachewithlbsdisk.sh

--- apache2-on-ubuntu-vm/install_apachewithlbsdisk.sh
+++ apache2-on-ubuntu-vm/install_apachewithlbsdisk.sh
@@ -18,6 +18,7 @@ vgcreate vg_app /dev/sdd1
 
 # Creación de los LVM
 lvcreate -L 10G -n lv_var vg_lbsdisks
+lvcreate -L 6G -n lv_home vg_lbsdisks
 lvcreate -L 8G -n lv_agent vg_lbsdisks
 lvcreate -l 100%FREE -n lv_tmp vg_lbsdisks
 lvcreate -l 100%FREE -n lv_apache vg_app
@@ -25,6 +26,7 @@ lvcreate -l 100%FREE -n lv_apache vg_app
 # Formateo de volumenes
 mkfs.ext4  /dev/mapper/vg_lbsdisks-lv_var
 mkfs.ext4  /dev/mapper/vg_lbsdisks-lv_tmp
+mkfs.ext4  /dev/mapper/vg_lbsdisks-lv_home
 mkfs.ext4  /dev/mapper/vg_lbsdisks-lv_agent
 mkfs.ext4  /dev/mapper/vg_app-lv_apache
 
@@ -39,18 +41,22 @@ mount  /dev/mapper/vg_lbsdisks-lv_var /tmp/vartmp/
 mount  /dev/mapper/vg_lbsdisks-lv_home /tmp/hometmp/
 
 # Copiar los archivos desde la carpeta /home,/var hacia carpetas en la ruta /tpm
+sudo rsync -avz /home/ /tmp/hometmp/ 
 sudo rsync -avz /var/ /tmp/vartmp/
 
+mount /dev/mapper/vg_lbsdisks-lv_home /home/  
 mount /dev/mapper/vg_lbsdisks-lv_var /var/
 mount /dev/mapper/vg_lbsdisks-lv_agent /agent/
 mount /dev/mapper/vg_app-lv_apache /var/www
- 
+
+rsync -avz /tmp/hometmp/ /home/  
 rsync -avz /tmp/vartmp/ /var/
 
 
 # Agregar montado de disco al archivo /etc/fstab
 echo '/dev/mapper/vg_lbsdisks-lv_tmp /tmp       ext4   defaults,nofail   1  2' >> /etc/fstab
 echo '/dev/mapper/vg_lbsdisks-lv_var /var       ext4   defaults,nofail    1  2' >> /etc/fstab
+echo '/dev/mapper/vg_lbsdisks-lv_home /home       ext4   defaults,nofail    1  2' >> /etc/fstab
 echo '/dev/mapper/vg_lbsdisks-lv_agent /agent       ext4   defaults,nofail    1  2' >> /etc/fstab
 echo '/dev/mapper/vg_app-lv_apache /var/www       ext4   defaults,nofail    1  2' >> /etc/fstab
 
